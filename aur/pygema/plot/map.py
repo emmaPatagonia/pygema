3ac11a84799df2c4c19920a4fad0c332a65aecd0
emmaPatagonia <38074940+emmaPatagonia@users.noreply.github.com>
2019-07-08 13:25:07 -0400

    update maps

--- aur/pygema/plot/map.py
+++ aur/pygema/plot/map.py
@@ -54,46 +54,38 @@ def plot_map(elon, wlon, nlat, slat, res='c', dpi=300, xpixels=800, add_holocene
 
   map.drawcoastlines(linewidth=0.8,zorder=5)
   map.drawcountries(linewidth=0.5, linestyle='-',zorder=2)
-  #map.fillcontinents(color='0.3', lake_color='steelblue',alpha=0.4,zorder=1)
-  #img = map.arcgisimage(service='NatGeo_World_Map', xpixels=xpixels, dpi=dpi, verbose=False, zorder=0)
-  #img = map.arcgisimage(service='World_Topo_Map', xpixels=xpixels, dpi=dpi, verbose=False, zorder=0)
-  #img = map.arcgisimage(service='ESRI_StreetMap_World_2D', xpixels=xpixels, dpi=dpi, verbose=False, zorder=0)
+
   map.arcgisimage(service='World_Shaded_Relief', xpixels=xpixels, dpi=dpi, verbose=False, zorder=1)
   img = map.arcgisimage(service='ESRI_Imagery_World_2D', xpixels=xpixels, dpi=dpi, verbose=False, zorder=0)
+  img.set_alpha(0.5)
+
   if add_events_pygemadb:
-    map.fillcontinents(color='0.3', lake_color='steelblue',alpha=0.4,zorder=1)
-    img.set_alpha(0.45)
-    color_stations = '0.8'
-    color_volcanoes = '0.8'
-    color_faults = 'k'
-    color_places = '0.8'
-    alpha=0.8
-  else:
-    img.set_alpha(0.6)
-    color_stations = 'y'
-    color_volcanoes = 'r'
-    color_faults = 'k'
-    color_places = 'c'
-    alpha = 1.0
+    map.fillcontinents(color='0.1', lake_color='steelblue',alpha=0.4,zorder=1000-1)
+
+  alpha = 1.0
+  color_stations = 'c'
+  color_volcanoes = 'r'
+  color_faults = 'k'
+  color_places = 'g'
 
   # add volcanoes
   if add_holocene_volcanoes:
     vnames, xvolcs, yvolcs = load_volcanoes()
     xvolcs, yvolcs = map(xvolcs,yvolcs)
-    ax.plot(xvolcs,yvolcs,marker='^',color='None',markeredgecolor=color_volcanoes,markeredgewidth=1.7,lw=0.,ms=5.,zorder=10,alpha=alpha, clip_on=True)
+    ax.plot(xvolcs,yvolcs,marker='^',color='None',markeredgecolor=color_volcanoes,markeredgewidth=1.5,lw=0.,ms=4.5,zorder=10,alpha=alpha, clip_on=True)
 
     if add_labels:
       for x,y,vname in zip(xvolcs,yvolcs,vnames):
         if vname == 'Tolhuaca':
-          ax.annotate('V. '+vname+'\n', (x, y), color='k', weight='bold', fontsize=5.5, ha='center', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate('Vn. '+vname+'\n', (x, y), color='k', weight='bold', fontsize=4.5, ha='center', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
         elif vname == 'Copahue':
-          ax.annotate('   V. '+vname, (x, y), color='k', weight='bold', fontsize=5.5, ha='left', va='top', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate('   Vn. '+vname, (x, y), color='k', weight='bold', fontsize=4.5, ha='left', va='top', clip_on=True, zorder=2000+3, fontstyle='normal')
         elif vname == 'Callaqui':
-          ax.annotate(' V. '+vname, (x, y), color='k', weight='bold', fontsize=5.5, ha='left', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate(' Vn. '+vname, (x, y), color='k', weight='bold', fontsize=4.5, ha='left', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
         elif vname == 'Trolon':
-          ax.annotate('V. '+vname+' ', (x, y), color='k', weight='bold', fontsize=5.5, ha='right', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate('Vn. '+vname+' ', (x, y), color='k', weight='bold', fontsize=4.5, ha='right', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
         elif vname == 'Lonquimay':
-          ax.annotate('  V. '+vname, (x, y), color='k', weight='bold', fontsize=5.5, ha='left', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate('  Vn. '+vname, (x, y), color='k', weight='bold', fontsize=4.5, ha='left', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
 
 
   # add stations
@@ -103,20 +95,20 @@ def plot_map(elon, wlon, nlat, slat, res='c', dpi=300, xpixels=800, add_holocene
       x,y = map(float(lon),float(lat))
       ax.scatter(x,y,marker='s',color='None', s=20, zorder=2000+2, alpha=alpha, clip_on=True, lw=1.4, edgecolors=color_stations)
       if add_labels:
-        ax.annotate(stat, (x, y), weight='bold', fontsize=5, ha='left', va='center', clip_on=True, zorder=2000+3)
+        ax.annotate("\n\n "+stat, (x, y), weight='bold', fontsize=5, ha='left', va='center', clip_on=True, zorder=2000+3)
 
 
   # add ralco
   if add_ralco:
     si,sj = map(-71.611683, -37.910428)
-    map.scatter(si, sj, c=color_places, linewidths=0.3, edgecolors='k', alpha=alpha, zorder=15, marker='X', s=40, clip_on=True)
+    map.scatter(si, sj, c=color_places, linewidths=0.3, edgecolors='k', alpha=alpha, zorder=15, marker='X', s=30, clip_on=True)
     if add_labels:
-      ax.annotate('Pangue  ',xy=(si,sj), ha='right', va='top', color='k',zorder=15,fontsize=6,fontweight='bold',xytext=(-0,-0),textcoords='offset points', fontstyle='normal', clip_on=True)
+      ax.annotate('Pangue  ',xy=(si,sj), ha='right', va='top', color='k',zorder=15,fontsize=4.5,fontweight='bold',xytext=(-0,-0),textcoords='offset points', fontstyle='normal', clip_on=True)
 
     si,sj = map(-71.475571, -38.046040)
-    map.scatter(si, sj, c=color_places, linewidths=0.3, edgecolors='k', alpha=alpha, zorder=15, marker='X', s=40, clip_on=True)
+    map.scatter(si, sj, c=color_places, linewidths=0.3, edgecolors='k', alpha=alpha, zorder=15, marker='X', s=30, clip_on=True)
     if add_labels:
-      ax.annotate('Ralco  ',xy=(si,sj), ha='right', va='top', color='k',zorder=15,fontsize=6,fontweight='bold',xytext=(-0,-0),textcoords='offset points', fontstyle='normal', clip_on=True)
+      ax.annotate('Ralco  ',xy=(si,sj), ha='right', va='top', color='k',zorder=15,fontsize=4.5,fontweight='bold',xytext=(-0,-0),textcoords='offset points', fontstyle='normal', clip_on=True)
 
 
   # add faults
@@ -128,10 +120,10 @@ def plot_map(elon, wlon, nlat, slat, res='c', dpi=300, xpixels=800, add_holocene
     for info, shape in zip(map.fallas_info, map.fallas):
       if info['Name'] == 'LOFS':
         x, y = zip(*shape) 
-        map.plot(x, y, marker=None, color=color_faults, alpha=alpha, linestyle='-', linewidth=0.8, zorder=9, clip_on=True)
+        map.plot(x, y, marker=None, color=color_faults, alpha=alpha, linestyle='-', linewidth=0.5, zorder=9, clip_on=True)
       elif info['Name'] == 'ATF':
         x, y = zip(*shape) 
-        map.plot(x, y, marker=None, color=color_faults, alpha=alpha, linestyle='--', linewidth=0.8, zorder=9, clip_on=True)
+        map.plot(x, y, marker=None, color=color_faults, alpha=alpha, linestyle='--', linewidth=0.5, zorder=9, clip_on=True)
 
 
 
@@ -141,8 +133,8 @@ def plot_map(elon, wlon, nlat, slat, res='c', dpi=300, xpixels=800, add_holocene
 
     zmin = 0
     zmax = 30
-    cmap = plt.cm.gnuplot_r
-    nlevels = 5
+    cmap = plt.cm.jet_r #plt.cm.jet_r  #plt.cm.gnuplot_r
+    nlevels = 10
     bounds = np.linspace(zmin, zmax,nlevels, endpoint=True)
     levels = np.linspace(zmin, zmax,nlevels, endpoint=True)
     norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
@@ -166,20 +158,20 @@ def plot_map(elon, wlon, nlat, slat, res='c', dpi=300, xpixels=800, add_holocene
 
       x.append(xi); y.append(yi); z.append(evdep); s.append( evmag**3*1.5 ); xerr.append(xi_err); yerr.append(yi_err); zerr.append(evdz)
 
-    ax.scatter(x,y,c=z, cmap=cmap, s=s, marker='o', norm=norm, zorder=100, alpha=1, clip_on=True, lw=0.3, edgecolors='k')
+    ax.scatter(x,y,c=z, cmap=cmap, s=s, marker='o', norm=norm, zorder=1000, alpha=1, clip_on=True, lw=0.3, edgecolors='k')
     #ax.errorbar(x, y, xerr=xerr, yerr=yerr, elinewidth=0.4, ecolor='0.4', capsize=1.7, capthick=0.4, linewidth=0, zorder=19)
 
 
 
   # add legend
-  s1 = plt.scatter([], [], c='None', linewidths=1.4, edgecolors=color_stations, alpha=alpha, zorder=1000, marker='s', s=25)
-  s2, = plt.plot([], [], c='None', markeredgecolor=color_volcanoes, markeredgewidth=1.4, lw=0., alpha=alpha, zorder=1000, marker='^', ms=5)
+  s1 = plt.scatter([], [], c='None', linewidths=1.4, edgecolors=color_stations, alpha=alpha, zorder=1000, marker='s', s=15)
+  s2, = plt.plot([], [], c='None', markeredgecolor=color_volcanoes, markeredgewidth=1.4, lw=0., alpha=alpha, zorder=1000, marker='^', ms=4)
   f1, = plt.plot([-100,-99], [-100,-98], c=color_faults, linewidth=0.8, alpha=alpha, zorder=1000, ls='-') # darkgoldenrod
   f2, = plt.plot([-100,-99], [-100,-98], c=color_faults, linewidth=0.8, alpha=alpha, zorder=1000, ls='--') # darkgoldenrod
 
-  leg = plt.legend([s2, s1, f1, f2], 
-                   [ 'Volcano', 'Seismic station', 'Liquiñe-Ofqui\nFault System', 'Biobio Fault' ], 
-                 fontsize=5, ncol=1, frameon=True, fancybox=True, shadow=False, framealpha=0.3, loc=4 ) 
+  leg = plt.legend([s1, s2, f1, f2], 
+                   [ 'Estación Sísmica', 'Volcán', 'Sistema de Fallas\nLiquiñe-Ofqui', 'Falla Biobio' ], 
+                 fontsize=4, ncol=1, frameon=True, fancybox=True, shadow=False, framealpha=0.6, loc=4 ) 
   leg.set_zorder(1000)
 
 
@@ -239,7 +231,7 @@ def plot_map(elon, wlon, nlat, slat, res='c', dpi=300, xpixels=800, add_holocene
     ax3.set_ylim(slat, nlat)
     labels = [ r"%.1f$^{\circ}$" % (item) for item in ax3.get_yticks().tolist() ]
     ax3.set_yticklabels(labels)
-    ax3.set_xlabel("Prof. (km)", fontsize=6)
+    ax3.set_xlabel("Profundidad (km)", fontsize=6)
     ax3.xaxis.set_label_position('top')
 
     x = []; y = []; z = []; s = []; xerr = []; yerr = []; zerr = []
@@ -263,13 +255,13 @@ def plot_map(elon, wlon, nlat, slat, res='c', dpi=300, xpixels=800, add_holocene
     ax3.scatter(z,y,c=z, cmap=cmap, s=s, marker='o', norm=norm, zorder=20, alpha=0.8, clip_on=True, lw=0.3, edgecolors='k')
     #ax3.errorbar(z, y, xerr=zerr, yerr=yerr, elinewidth=0.4, ecolor='0.4', capsize=1.7, capthick=0.4, linewidth=0, zorder=19)
 
-    ml2 = plt.scatter([],[], marker='o',color='w', s=2.0**3*1.5, zorder=2000+2, alpha=1, clip_on=True, lw=1, edgecolors='k')
-    ml3 = plt.scatter([],[], marker='o',color='w', s=3.0**3*1.5, zorder=2000+2, alpha=1, clip_on=True, lw=1, edgecolors='k')
-    ml4 = plt.scatter([],[], marker='o',color='w', s=4.0**3*1.5, zorder=2000+2, alpha=1, clip_on=True, lw=1, edgecolors='k')
-    ml5 = plt.scatter([],[], marker='o',color='w', s=5.0**3*1.5, zorder=2000+2, alpha=1, clip_on=True, lw=1, edgecolors='k')
+    ml2 = plt.scatter([],[], marker='o',color='w', s=1.5**3*1.5, zorder=2000+2, alpha=1, clip_on=True, lw=1, edgecolors='k')
+    ml3 = plt.scatter([],[], marker='o',color='w', s=2.5**3*1.5, zorder=2000+2, alpha=1, clip_on=True, lw=1, edgecolors='k')
+    ml4 = plt.scatter([],[], marker='o',color='w', s=3.5**3*1.5, zorder=2000+2, alpha=1, clip_on=True, lw=1, edgecolors='k')
+    ml5 = plt.scatter([],[], marker='o',color='w', s=4.5**3*1.5, zorder=2000+2, alpha=1, clip_on=True, lw=1, edgecolors='k')
 
     leg = plt.legend([ml5, ml4, ml3, ml2, s1, s2, f1, f2], 
-                 ['5.0', '4.0', '3.0', '2.0' ], title=r"Mag. (M$_l$)", title_fontsize=6, labelspacing=1.2, 
+                 ['4.5', '3.5', '2.5', '1.5' ], title=r"M$_l$", title_fontsize=6, labelspacing=1.2, 
                  fontsize=6, ncol=1, frameon=True, fancybox=True, shadow=False, framealpha=0.2, bbox_to_anchor=(0.55, -0.03) ) 
     leg.set_zorder(1000)
     frame = leg.get_frame()
@@ -280,7 +272,7 @@ def plot_map(elon, wlon, nlat, slat, res='c', dpi=300, xpixels=800, add_holocene
     cax = fig.add_axes([0.87, 0.108, 0.017, 0.14])
     cb1 = plt.colorbar(sm, format='%i', extend='neither', norm=norm, spacing='proportional', orientation='vertical', cax=cax, ticks = np.linspace(zmin, zmax, 5)) 
     cb1.ax.invert_yaxis()
-    cb1.set_label('Prof. (km)', size=6)
+    cb1.set_label('Profundidad (km)', size=6)
     for j in cb1.ax.get_yticklabels(): j.set_fontsize(6)
 
 
@@ -355,15 +347,15 @@ def plot_map_event(evlon, evlat, evdep, dlon=0.3, dlat=0.3, res='c', dpi=300, xp
     if add_labels:
       for x,y,vname in zip(xvolcs,yvolcs,vnames):
         if vname == 'Tolhuaca':
-          ax.annotate('V. '+vname+'\n', (x, y), color='k', weight='bold', fontsize=5.5, ha='center', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate('V. '+vname+'\n', (x, y), color='k', weight='bold', fontsize=4.5, ha='center', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
         elif vname == 'Copahue':
-          ax.annotate('   V. '+vname, (x, y), color='k', weight='bold', fontsize=5.5, ha='left', va='top', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate('   V. '+vname, (x, y), color='k', weight='bold', fontsize=4.5, ha='left', va='top', clip_on=True, zorder=2000+3, fontstyle='normal')
         elif vname == 'Callaqui':
-          ax.annotate(' V. '+vname, (x, y), color='k', weight='bold', fontsize=5.5, ha='left', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate(' V. '+vname, (x, y), color='k', weight='bold', fontsize=4.5, ha='left', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
         elif vname == 'Trolon':
-          ax.annotate('V. '+vname+' ', (x, y), color='k', weight='bold', fontsize=5.5, ha='right', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate('V. '+vname+' ', (x, y), color='k', weight='bold', fontsize=4.5, ha='right', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
         elif vname == 'Lonquimay':
-          ax.annotate('  V. '+vname, (x, y), color='k', weight='bold', fontsize=5.5, ha='left', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
+          ax.annotate('  V. '+vname, (x, y), color='k', weight='bold', fontsize=4.5, ha='left', va='bottom', clip_on=True, zorder=2000+3, fontstyle='normal')
 
 
   # add stations
@@ -373,7 +365,7 @@ def plot_map_event(evlon, evlat, evdep, dlon=0.3, dlat=0.3, res='c', dpi=300, xp
       x,y = map(float(lon),float(lat))
       ax.scatter(x,y,marker='s',color='None', s=20, zorder=2000+2, alpha=alpha, clip_on=True, lw=1.4, edgecolors=color_stations)
       if add_labels:
-        ax.annotate(stat, (x, y), weight='bold', fontsize=5, ha='left', va='center', clip_on=True, zorder=2000+3)
+        ax.annotate(stat, (x, y), weight='bold', fontsize=4.5, ha='left', va='center', clip_on=True, zorder=2000+3)
 
 
   # add ralco
@@ -381,12 +373,12 @@ def plot_map_event(evlon, evlat, evdep, dlon=0.3, dlat=0.3, res='c', dpi=300, xp
     si,sj = map(-71.611683, -37.910428)
     map.scatter(si, sj, c=color_places, linewidths=0.3, edgecolors='k', alpha=alpha, zorder=15, marker='X', s=40, clip_on=True)
     if add_labels:
-      ax.annotate('Pangue  ',xy=(si,sj), ha='right', va='top', color='k',zorder=15,fontsize=6,fontweight='bold',xytext=(-0,-0),textcoords='offset points', fontstyle='normal', clip_on=True)
+      ax.annotate('Pangue  ',xy=(si,sj), ha='right', va='top', color='k',zorder=15,fontsize=4.5,fontweight='bold',xytext=(-0,-0),textcoords='offset points', fontstyle='normal', clip_on=True)
 
     si,sj = map(-71.475571, -38.046040)
     map.scatter(si, sj, c=color_places, linewidths=0.3, edgecolors='k', alpha=alpha, zorder=15, marker='X', s=40, clip_on=True)
     if add_labels:
-      ax.annotate('Ralco  ',xy=(si,sj), ha='right', va='top', color='k',zorder=15,fontsize=6,fontweight='bold',xytext=(-0,-0),textcoords='offset points', fontstyle='normal', clip_on=True)
+      ax.annotate('Ralco  ',xy=(si,sj), ha='right', va='top', color='k',zorder=15,fontsize=4.5,fontweight='bold',xytext=(-0,-0),textcoords='offset points', fontstyle='normal', clip_on=True)
 
 
   # add faults
